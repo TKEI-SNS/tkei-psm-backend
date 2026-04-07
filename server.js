b8bf22d349f9556fc85677a43b51c08287649802
TKEI-SNS <sohail.shaikh.x@tkelevator.com>
2026-04-07 16:40:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const calcService = new CalculationService(
 // NOTE: createTransport (not createTransporter)
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: 465,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.SMTP_USER,
